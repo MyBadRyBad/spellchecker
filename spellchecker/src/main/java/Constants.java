d46b981444ec101dfe516c34a0744b2c63cb7c8d
Ryan B Badilla <ryan.badilla@gmail.com>
2017-06-01 14:57:50 -0700

    added additional word.txt containing more words

--- spellchecker/src/main/java/Constants.java
+++ spellchecker/src/main/java/Constants.java
@@ -4,12 +4,12 @@
 public final class Constants {
 
     public final static String[] WORDS_FILE_PATHS = {
+            "/src/main/resources/words.txt",
             "/usr/share/dict/words",
             "/usr/dict/words"
     };
 
     public final static String NO_SUGGESTION_RESPONSE = "NO SUGGESTION";
-
 
     public final static String ERR_USAGE_STRING = "usage: java spellchecker word";
     public final static String ERR_WORDS_FILE_NOT_FOUND = "Error: Could not find valid words file.";
